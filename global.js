6ede34ad742180deba7b0ce580384066753b6c5c
Jeremy <jeremy39c@gmail.com>
2025-01-24 08:22:44 -0800

    updated global.js

--- global.js
+++ global.js
@@ -31,4 +31,14 @@ for (let p of pages) {
     a.href = url;
     a.textContent= title;
     nav.append(a);
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+    // a.classList.toggle(
+        // 'current',
+        // a.host === location.host && a.pathname === location.pathname
+    // );
+    if (a.host !== location.host) {
+        a.target = "_blank";
+    }
 }
