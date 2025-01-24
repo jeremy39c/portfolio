35e0c2d2e7d88a71c150b337b72e150f3ee3ed0e
Jeremy <jeremy39c@gmail.com>
2025-01-24 07:53:37 -0800

    edited global.js to add navigation menu

--- global.js
+++ global.js
@@ -4,8 +4,28 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a");
-let currentLink = navLinks.find(
-    (a) => a.host == location.host && a.pathname == location.pathname
-);
-currentLink?.classList.add('current');
+// let navLinks = $$("nav a");
+// let currentLink = navLinks.find(
+//     (a) => a.host == location.host && a.pathname == location.pathname
+// );
+// currentLink?.classList.add('current');
+
+let pages = [
+    {url: '', title: 'Home'},
+    {url: 'projects/', title: 'Projects'},
+    {url: 'contact/', title: 'Contact'},
+    {url: 'https://github.com/jeremy39c', title: 'Profile'},
+    {url: 'resume/', title: 'Resume'},
+];
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+if(!ARE_WE_HOME && !url.startsWith('http')) {
+    url = '../' + url;
+}
+// url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
