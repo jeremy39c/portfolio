fdf2ad85c9f63eb47a16a2f19d0cf876e2d3a85b
Jeremy <jeremy39c@gmail.com>
2025-01-24 08:25:13 -0800

    updated global.js

--- global.js
+++ global.js
@@ -30,7 +30,6 @@ for (let p of pages) {
     let a = document.createElement('a');
     a.href = url;
     a.textContent= title;
-    nav.append(a);
     if (a.host === location.host && a.pathname === location.pathname) {
         a.classList.add('current');
     }
@@ -41,4 +40,5 @@ for (let p of pages) {
     if (a.host !== location.host) {
         a.target = "_blank";
     }
+    nav.append(a);
 }
