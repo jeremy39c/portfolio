a6bec8c831a608282de5a552db22a4a42353683a
Jeremy <jeremy39c@gmail.com>
2025-01-29 15:30:26 -0800

    updated with changes from lab 4

--- global.js
+++ global.js
@@ -81,3 +81,33 @@ form?.addEventListener('submit', function (event) {
     }
     location.href = url.slice(0, -1);
 });
+
+export async function fetchJSON(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch projects: ${response.statusText}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching or parsing JSON data:', error);
+    } 
+}
+
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+    containerElement.innerHTML = '';
+    for (const proj of project) {
+        const article = document.createElement('article');
+        article.innerHTML = `
+            <${headingLevel}>${proj.title}</${headingLevel}>
+            <img src="${proj.image}" alt="${proj.title}">
+            <p>${proj.description}</p>
+        `;
+        containerElement.appendChild(article);
+    }
+}
+
+export async function fetchGitHubData(username) {
+    return fetchJSON(`https://api.github.com/users/${username}`);
+}
