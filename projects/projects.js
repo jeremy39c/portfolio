3c958cd8b65a04e4d0ba2b7e6884dfcb408bf86a
Jeremy <jeremy39c@gmail.com>
2025-02-07 11:42:44 -0800

    Began to draw static pie chart with D3

--- projects/projects.js
+++ projects/projects.js
@@ -1,3 +1,4 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
@@ -8,3 +9,20 @@ renderProjects(projects, projectsContainer, 'h2');
 const title = document.querySelector('.projects-title');
 let projectsCount = projects.length;
 title.textContent =  `${projectsCount} ${title.textContent}`;
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let data = [1, 2];
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
+let arcs = arcData.map((d) => arcGenerator(d));
+let colors = ['gold', 'purple'];
+
+arcs.forEach((arc, idx) => {
+    (d3
+        .select('svg')
+        .append('path')
+        .attr('d', arc)
+        .attr('fill', colors[idx])
+    );
+})
