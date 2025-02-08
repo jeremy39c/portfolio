d356c125ff3de218c775a7694a71f00ad87bbeba
Jeremy <jeremy39c@gmail.com>
2025-02-07 20:56:10 -0800

    turned pie chart into filtering UI for projects

--- projects/projects.js
+++ projects/projects.js
@@ -1,7 +1,7 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 import { fetchJSON, renderProjects } from '../global.js';
 
-const projects = await fetchJSON('../lib/projects.json');
+let projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 
 renderProjects(projects, projectsContainer, 'h2');
@@ -10,9 +10,13 @@ const title = document.querySelector('.projects-title');
 let projectsCount = projects.length;
 title.textContent =  `${projectsCount} ${title.textContent}`;
 
-function renderPieChart(projectsGiven) {
-    let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-    
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+let selectedIndex = -1;
+
+function renderPieChart(projectsGiven) {    
     let newRolledData = d3.rollups(
         projectsGiven,
         (v) => v.length,
@@ -32,14 +36,13 @@ function renderPieChart(projectsGiven) {
     let newLegend = d3.select('.legend');
     newLegend.selectAll('li').remove();
 
-    let colors = d3.scaleOrdinal(d3.schemeTableau10);
     newArcs.forEach((arc, idx) => {
         (newSVG
             .append('path')
             .attr('d', arc)
             .attr('fill', colors(idx))
         );
-    })
+    });
     newData.forEach((d, idx) => {
         (newLegend
             .append('li')
@@ -47,7 +50,44 @@ function renderPieChart(projectsGiven) {
             .attr('class', 'legend-item')
             .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`)
         );
-    })
+    });
+    newSVG.selectAll('path').remove();
+    newArcs.forEach((arc, i) => {
+        (newSVG
+            .append('path')
+            .attr('d', arc)
+            .attr('fill', colors(i))
+            .on('click', () => {
+                selectedIndex = selectedIndex === i ? -1 : i;
+            
+            (newSVG
+                .selectAll('path')
+                .attr('class', (_, idx) => {
+                    if (idx == selectedIndex) {
+                        return 'selected';
+                    }
+                }));
+            
+            (newLegend
+                .selectAll('li')
+                .attr('class', (_, idx) => {
+                    if (idx == selectedIndex) {
+                        return 'legend-item selected';
+                    }
+                    else {
+                        return 'legend-item';
+                    }
+                }));
+            if (selectedIndex === -1) {
+                renderProjects(projectsGiven, projectsContainer, 'h2');
+            }
+            else {
+                let filteredProjects = projectsGiven.filter((project) => project.year == newData[selectedIndex].label);
+                renderProjects(filteredProjects, projectsContainer, 'h2');
+                projects = filteredProjects;
+            }
+        }));
+    });
 }
 
 renderPieChart(projects);
